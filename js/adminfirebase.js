6cae204b8ef6dbfe5782df79638a0a030715bb14
Klaas Van Mechelen <klaasvanmechelengame@gmail.com>
2025-02-08 14:24:47 +0100

    try fix firebase config

--- js/adminfirebase.js
+++ js/adminfirebase.js
@@ -1,11 +1,10 @@
-// adminfirebase.js
-
 export const firebaseConfig = {
-  apiKey: process.env.ADMIN_FIREBASE_API_KEY,
-  authDomain: process.env.ADMIN_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.ADMIN_FIREBASE_DATABASE_URL,
-  projectId: process.env.ADMIN_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.ADMIN_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.ADMIN_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.ADMIN_FIREBASE_APP_ID,
+  apiKey: "{{ADMIN_FIREBASE_API_KEY}}", 
+  authDomain: "{{ADMIN_FIREBASE_AUTH_DOMAIN}}",
+  databaseURL: "{{ADMIN_FIREBASE_DATABASE_URL}}",
+  projectId: "{{ADMIN_FIREBASE_PROJECT_ID}}",
+  storageBucket: "{{ADMIN_FIREBASE_STORAGE_BUCKET}}",
+  messagingSenderId: "{{ADMIN_FIREBASE_MESSAGING_SENDER_ID}}",
+  appId: "{{ADMIN_FIREBASE_APP_ID}}",
+  measurementId: "{{ADMIN_FIREBASE_MEASUREMENT_ID}}"
 };
