21137b13fe5e94e02b191d085bbeb5aa5dd0aa61
Klaas Van Mechelen <klaasvanmechelengame@gmail.com>
2025-02-08 13:28:08 +0100

    login screen

---
+++ js/adminfirebase.js
@@ -0,0 +1,13 @@
+// firebase.js
+const firebaseConfig = {
+  apiKey: process.env.ADMIN_FIREBASE_API_KEY,
+  authDomain: process.env.ADMIN_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.ADMIN_FIREBASE_DATABASE_URL,
+  projectId: process.env.ADMIN_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.ADMIN_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.ADMIN_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.ADMIN_FIREBASE_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
