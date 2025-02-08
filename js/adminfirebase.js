0e06ec6326de5ae4d4bd76b822f707a19ba3dbd4
klaasvm <klaasvm@users.noreply.github.com>
2025-02-08 13:39:41 +0000

    deploy: 0a2a6b06c4cf03607d367a733c260ccfca361222

---
+++ js/adminfirebase.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAwgAdyms6OCibPHkVdf7bWlxepr2XY2hg", 
+  authDomain: "admin-65859.firebaseapp.com",
+  databaseURL: "https://admin-65859-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "admin-65859",
+  storageBucket: "admin-65859.firebasestorage.app",
+  messagingSenderId: "",
+  appId: "1:341190555043:web:987da42c601ffd77d5d616",
+  measurementId: ""
+};
